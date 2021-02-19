69055367327fae7955582933ef1a18a0b75f1407
Michael Montabano <mcmontalbano@schooner1.oscer.ou.edu>
2021-02-19 05:25:37 -0600

    hi

--- batch.sh
+++ batch.sh
@@ -17,5 +17,5 @@
 # Do not change this line unless you have your own python/tensorflow/keras set up
 source ~fagg/pythonenv/tensorflow/bin/activate
 # Change this line to start an instance of your experiment
-python nn_2_2.py
+python nn_1.py
 
