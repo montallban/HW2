0e467096aa2d4be08133d792869383746a735367
mmont <54285521+montallban@users.noreply.github.com>
2021-02-28 21:50:02 -0600

    Add files via upload

--- batch.sh
+++ batch.sh
@@ -6,13 +6,13 @@
 #SBATCH --ntasks=1
 # memory in MB
 #SBATCH --mem=1024
-#SBATCH --output=homework2/HW2/testing_%J_err.txt
-#SBATCH --error=homework2/HW2/testing_%J_stderr.txt
+#SBATCH --output=results/testing_%J_err.txt
+#SBATCH --error=results/testing_%J_stderr.txt
 #SBATCH --time=00:00:05
 #SBATCH --job-name=bmi_test
 #SBATCH --mail-user=michael.montalbano@ou.edu
 #SBATCH --mail-type=ALL
-#SBATCH --chdir=/home/mcmontalbano/homework2/HW2/results/
+#SBATCH --chdir=/home/mcmontalbano/homework2/HW2
 #
 #################################################
 # Do not change this line unless you have your own python/tensorflow/keras set up
